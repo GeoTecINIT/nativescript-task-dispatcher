df5a36a811a26c21b563e8ae611d8f56ab2347bc
Alberto González <alberto.gonzalez@uji.es>
2020-04-16 20:48:10 +0200

    Add a task runner able to run task one by one

---
+++ demo/app/tests/api/tasks/runners/single-task-runner.ts
@@ -0,0 +1,134 @@
+import { setTasks } from "nativescript-task-dispatcher/api/tasks/provider";
+import { testTasks } from "..";
+import { createPlannedTaskStoreMock } from "../../persistence";
+import {
+    PlannedTask,
+    PlanningType,
+} from "nativescript-task-dispatcher/api/tasks/planner/planned-task";
+import { SingleTaskRunner } from "nativescript-task-dispatcher/api/tasks/runners/single-task-runner";
+import {
+    CoreEvent,
+    emit,
+    createEvent,
+    PlatformEvent,
+} from "nativescript-task-dispatcher/api/events";
+
+describe("Single task runner", () => {
+    setTasks(testTasks);
+    const taskStore = createPlannedTaskStoreMock();
+
+    const dummyTask = new PlannedTask(PlanningType.Alarm, {
+        name: "dummyTask",
+        startAt: -1,
+        interval: 60000,
+        recurrent: true,
+        params: {},
+    });
+    const dummyOneShotTask = new PlannedTask(PlanningType.Alarm, {
+        name: "dummyTask",
+        startAt: -1,
+        interval: 60000,
+        recurrent: false,
+        params: {},
+    });
+    const dummyImmediateTask = new PlannedTask(PlanningType.Immediate, {
+        name: "dummyTask",
+        startAt: -1,
+        interval: 0,
+        recurrent: false,
+        params: {},
+    });
+    const failedTask = new PlannedTask(PlanningType.Alarm, {
+        name: "failedTask",
+        startAt: -1,
+        interval: 60000,
+        recurrent: false,
+        params: {},
+    });
+    failedTask.errorCount = 1;
+    const timeoutTask = new PlannedTask(PlanningType.Alarm, {
+        name: "timeoutTask",
+        startAt: -1,
+        interval: 60000,
+        recurrent: false,
+        params: {},
+    });
+    timeoutTask.timeoutCount = 1;
+
+    let startEvent: PlatformEvent;
+    let timeoutEvent: PlatformEvent;
+
+    const taskRunner = new SingleTaskRunner(taskStore);
+
+    beforeEach(() => {
+        startEvent = createEvent(CoreEvent.TaskExecutionStarted);
+        timeoutEvent = createEvent(CoreEvent.TaskExecutionTimedOut, {
+            id: startEvent.id,
+        });
+
+        spyOn(taskStore, "updateLastRun").and.returnValue(Promise.resolve());
+        spyOn(taskStore, "increaseErrorCount").and.returnValue(
+            Promise.resolve()
+        );
+        spyOn(taskStore, "increaseTimeoutCount").and.returnValue(
+            Promise.resolve()
+        );
+        spyOn(taskStore, "delete").and.returnValue(Promise.resolve());
+    });
+
+    it("executes a task successfully", async () => {
+        await taskRunner.run(dummyTask, startEvent);
+
+        expect(taskStore.updateLastRun).toHaveBeenCalledWith(
+            dummyTask.id,
+            jasmine.any(Number)
+        );
+        expect(taskStore.delete).not.toHaveBeenCalled();
+    });
+
+    it("executes a one-shot task successfully", async () => {
+        spyOn(taskStore, "get").and.returnValue(
+            Promise.resolve(dummyOneShotTask)
+        );
+
+        await taskRunner.run(dummyOneShotTask, startEvent);
+
+        expect(taskStore.updateLastRun).toHaveBeenCalledWith(
+            dummyOneShotTask.id,
+            jasmine.any(Number)
+        );
+        expect(taskStore.delete).toHaveBeenCalled();
+    });
+
+    it("executes a immediate task successfully", async () => {
+        await taskRunner.run(dummyImmediateTask, startEvent);
+
+        expect(taskStore.updateLastRun).toHaveBeenCalledWith(
+            dummyImmediateTask.id,
+            jasmine.any(Number)
+        );
+        expect(taskStore.delete).not.toHaveBeenCalled();
+    });
+
+    it("increases the error count of a task that has failed", async () => {
+        spyOn(taskStore, "get").and.returnValue(Promise.resolve(failedTask));
+
+        await taskRunner.run(failedTask, startEvent);
+
+        expect(taskStore.increaseErrorCount).toHaveBeenCalledWith(
+            failedTask.id
+        );
+        expect(taskStore.delete).not.toHaveBeenCalled();
+    });
+
+    it("increases the timeout count of a task that has failed", async () => {
+        setTimeout(() => emit(timeoutEvent), 200);
+        spyOn(taskStore, "get").and.returnValue(Promise.resolve(timeoutTask));
+        await taskRunner.run(timeoutTask, startEvent);
+
+        expect(taskStore.increaseTimeoutCount).toHaveBeenCalledWith(
+            timeoutTask.id
+        );
+        expect(taskStore.delete).not.toHaveBeenCalled();
+    });
+});
---
+++ src/api/tasks/runners/single-task-runner.ts
@@ -0,0 +1,120 @@
+import { PlannedTasksStore } from '../../persistence/planned-tasks-store';
+import { PlannedTask } from '../planner/planned-task';
+import { getTask } from '../provider';
+import { Task, TaskParams } from '../task';
+import { PlatformEvent, on, CoreEvent, off } from '../../events';
+import { Logger, getLogger } from '../../utils/logger';
+
+const FAILURE_THRESHOLD = 3;
+
+export class SingleTaskRunner {
+  private logger: Logger;
+
+  constructor(private taskStore: PlannedTasksStore) {
+    this.logger = getLogger('SingleTaskRunner');
+  }
+
+  async run(
+    plannedTask: PlannedTask,
+    startEvent: PlatformEvent
+  ): Promise<void> {
+    const { name, id, params } = plannedTask;
+    const task = getTask(name);
+
+    await this.taskStore.updateLastRun(id, new Date().getTime());
+
+    try {
+      const parameterizedTask = new ParameterizedTask(task, params, startEvent);
+      await this.runWithTimeout(id, parameterizedTask, startEvent.id);
+    } catch (error) {
+      await this.taskStore.increaseErrorCount(id);
+    }
+
+    if (!plannedTask.recurrent && plannedTask.interval > 0) {
+      await this.handleOneShotTask(plannedTask.id);
+    }
+  }
+
+  private runWithTimeout(
+    id: string,
+    task: ParameterizedTask,
+    startEventId: string
+  ): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const listenerId = on(CoreEvent.TaskExecutionTimedOut, (evt) => {
+        if (evt.id === startEventId) {
+          off(CoreEvent.TaskExecutionTimedOut, listenerId);
+          task.cancel();
+          this.taskStore.increaseTimeoutCount(id).then(() => resolve());
+        }
+      });
+
+      let taskAlreadyRun = false;
+      let taskChainFinished = false;
+      this.waitForTaskChainToFinish(startEventId).then(() => {
+        taskChainFinished = true;
+        if (taskAlreadyRun) {
+          resolve();
+        }
+      });
+
+      task
+        .run()
+        .then(() => {
+          taskAlreadyRun = true;
+          off(CoreEvent.TaskExecutionTimedOut, listenerId);
+          if (taskChainFinished) {
+            resolve();
+          }
+        })
+        .catch((err) => {
+          off(CoreEvent.TaskExecutionTimedOut, listenerId);
+          reject(err);
+        });
+    });
+  }
+
+  private waitForTaskChainToFinish(startEventId: string): Promise<void> {
+    return new Promise((resolve) => {
+      const listenerId = on(CoreEvent.TaskChainFinished, (chainFinishedEvt) => {
+        if (chainFinishedEvt.id === startEventId) {
+          off(CoreEvent.TaskChainFinished, listenerId);
+          resolve();
+        }
+      });
+    });
+  }
+
+  private async handleOneShotTask(id: string): Promise<void> {
+    const { errorCount, timeoutCount } = await this.taskStore.get(id);
+
+    if (errorCount === 0 && timeoutCount === 0) {
+      await this.taskStore.delete(id);
+    } else if (
+      errorCount > FAILURE_THRESHOLD ||
+      timeoutCount > FAILURE_THRESHOLD
+    ) {
+      this.logger.warn(
+        `One-shot planned task id:${id} discarded after {errorCount:${errorCount}, timeoutCount:${timeoutCount}}`
+      );
+      await this.taskStore.delete(id);
+    }
+  }
+}
+
+// tslint:disable-next-line:max-classes-per-file
+class ParameterizedTask {
+  constructor(
+    private task: Task,
+    private taskParams: TaskParams,
+    private startEvent: PlatformEvent
+  ) {}
+
+  run(): Promise<void> {
+    return this.task.run(this.taskParams, this.startEvent);
+  }
+
+  cancel() {
+    this.task.cancel();
+  }
+}
