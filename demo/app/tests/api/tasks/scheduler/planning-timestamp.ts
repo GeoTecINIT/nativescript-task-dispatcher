f3b85a08c2ca0f6864c199382c6e3dda9152f1bc
Alberto González <alberto.gonzalez@uji.es>
2020-04-16 21:33:33 +0200

    Add scheduler debugging tools

---
+++ demo/app/tests/api/tasks/scheduler/planning-timestamp.ts
@@ -0,0 +1,37 @@
+import {
+    getNumber,
+    setNumber,
+} from "tns-core-modules/application-settings/application-settings";
+
+const PREVIOUS_PLANNING_TIMESTAMP = "PREVIOUS_PLANNING_TIMESTAMP";
+const CURRENT_PLANNING_TIMESTAMP = "CURRENT_PLANNING_TIMESTAMP";
+
+class PlanningTimestamp {
+    private _previous: number = getNumber(PREVIOUS_PLANNING_TIMESTAMP, -1);
+    private _current: number = getNumber(CURRENT_PLANNING_TIMESTAMP, -1);
+
+    get previous(): number {
+        return this._previous;
+    }
+
+    get current(): number {
+        return this._current;
+    }
+
+    updateCurrent(): void {
+        this.setPrevious(this.current);
+        this.setCurrent(new Date().getTime());
+    }
+
+    private setPrevious(value: number) {
+        this._previous = value;
+        setNumber(PREVIOUS_PLANNING_TIMESTAMP, value);
+    }
+
+    private setCurrent(value: number) {
+        this._current = value;
+        setNumber(CURRENT_PLANNING_TIMESTAMP, value);
+    }
+}
+
+export const planningTimestamp = new PlanningTimestamp();
