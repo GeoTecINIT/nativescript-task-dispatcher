839573df3aee7ef4e2106353439617036a1d14f2
Alberto González <alberto.gonzalez@uji.es>
2020-04-16 20:28:59 +0200

    Add task provider

---
+++ demo/app/tests/api/tasks/provider.ts
@@ -0,0 +1,31 @@
+import {
+    getTask,
+    TaskNotFoundError,
+    setTasks,
+    registerTasks,
+} from "nativescript-task-dispatcher/api/tasks/provider";
+import { testTasks } from ".";
+import { SimpleTask } from "nativescript-task-dispatcher/api/tasks/simple-task";
+
+describe("Task provider", () => {
+    setTasks(testTasks);
+
+    it("throws an error when task os unknown", () => {
+        const name = "patata";
+        expect(() => getTask(name)).toThrow(new TaskNotFoundError(name));
+    });
+
+    it("returns a dummy task", () => {
+        const name = "dummyTask";
+        const expectedTask = testTasks.find((task) => task.name === name);
+        expect(getTask(name)).toBe(expectedTask);
+    });
+
+    it("allows to register new tasks", () => {
+        const newTask1 = new SimpleTask("aNewTask", async () => null);
+        const newTask2 = new SimpleTask("anotherNewTask", async () => null);
+        registerTasks([newTask1, newTask2]);
+        expect(getTask(newTask1.name)).toBe(newTask1);
+        expect(getTask(newTask2.name)).toBe(newTask2);
+    });
+});
---
+++ src/api/tasks/provider.ts
@@ -0,0 +1,42 @@
+import { Task } from './task';
+
+interface Tasks {
+  [key: string]: Task;
+}
+
+let tasks: Tasks = {};
+
+export function getTask(name: string): Task {
+  checkIfTaskExists(name);
+
+  return tasks[name];
+}
+
+export function checkIfTaskExists(name: string) {
+  const task = tasks[name];
+  if (!task) {
+    throw new TaskNotFoundError(name);
+  }
+}
+
+export function registerTasks(newTasks: Array<Task>) {
+  for (const task of newTasks) {
+    if (tasks[task.name]) {
+      throw new Error(
+        `Task (${task.name}) name collides with an existing task. Cannot guarantee the correct operation of the system`
+      );
+    }
+    tasks[task.name] = task;
+  }
+}
+
+export function setTasks(t: Array<Task>) {
+  tasks = {};
+  registerTasks(t);
+}
+
+export class TaskNotFoundError extends Error {
+  constructor(name: string) {
+    super(`Task not found: ${name}`);
+  }
+}
