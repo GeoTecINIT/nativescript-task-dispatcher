c0018c2a3c898b8a0c3082394e9b729d02d43ee5
Alberto González <alberto.gonzalez@uji.es>
2020-04-23 17:54:54 +0200

    Add android watchdog handler

--- src/task-dispatcher.android.ts
+++ src/task-dispatcher.android.ts
@@ -1,13 +1,36 @@
 import { Common } from "./task-dispatcher.common";
 import { BootReceiver } from "./internal/android/boot-receiver.android";
+import { WatchdogReceiver } from "./internal/tasks/scheduler/android/alarms/watchdog/receiver.android";
 
 const bootReceiver = new BootReceiver();
+const watchdogReceiver = new WatchdogReceiver();
 
 class TaskDispatcher extends Common {
   public init(): void {
+    this.wireUpNativeComponents();
+  }
+
+  private wireUpNativeComponents() {
+    this.wireUpBootReceiver();
+    this.wireUpWatchdogReceiver();
+  }
+
+  private wireUpBootReceiver() {
     es.uji.geotec.taskdispatcher.BootReceiver.setBootReceiverDelegate(
       new es.uji.geotec.taskdispatcher.BootReceiverDelegate({
-        onReceive: (context, intent) => bootReceiver.onReceive(context, intent),
+        onReceive(context, intent) {
+          bootReceiver.onReceive(context, intent);
+        },
+      })
+    );
+  }
+
+  private wireUpWatchdogReceiver() {
+    es.uji.geotec.taskdispatcher.alarms.WatchdogReceiver.setWatchdogReceiverDelegate(
+      new es.uji.geotec.taskdispatcher.alarms.WatchdogReceiverDelegate({
+        onReceive(context, intent) {
+          watchdogReceiver.onReceive(context, intent);
+        },
       })
     );
   }
