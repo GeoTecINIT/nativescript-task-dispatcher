f67051037443bafaefa88adedbe6dc8ed7824330
Alberto González <alberto.gonzalez@uji.es>
2020-04-23 17:41:12 +0200

    Update android boot receiver

--- src/internal/android/boot-receiver.android.ts
+++ src/internal/android/boot-receiver.android.ts
@@ -1,10 +1,18 @@
+import { AndroidAlarmScheduler } from "../tasks/scheduler/android/alarms/alarm/scheduler.android";
+import { getLogger } from "../utils/logger";
+
 export class BootReceiver
   implements es.uji.geotec.taskdispatcher.BootReceiverDelegate {
   onReceive(context: android.content.Context, intent: android.content.Intent) {
-    this.handleOnReceive();
-  }
+    const logger = getLogger("BootReceiver");
+    logger.info("Performing boot initializations");
 
-  private handleOnReceive() {
-    console.log("Boot completed!");
+    const alarmScheduler = new AndroidAlarmScheduler();
+    alarmScheduler
+      .setup()
+      .then(() => logger.debug("Alarm setup has run"))
+      .catch((err) => {
+        logger.error(`${err}`);
+      });
   }
 }
