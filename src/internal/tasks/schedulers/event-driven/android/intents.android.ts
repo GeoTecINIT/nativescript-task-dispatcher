174d10e01287855d229b275083aec05a05801420
Alberto González <alberto.gonzalez@uji.es>
2020-05-15 16:27:59 +0200

    Add task chain runner intent creator and unpacker

---
+++ src/internal/tasks/schedulers/event-driven/android/intents.android.ts
@@ -0,0 +1,47 @@
+import { createLibComponentIntent } from "../../intent-tools.android";
+import { EventData } from "../../../../events/events";
+
+export function createTaskChainRunnerServiceIntent(
+  appContext: android.content.Context,
+  params: TaskChainRunnerParams
+) {
+  const intent = createLibComponentIntent(appContext, {
+    relativeClassPath: ".runners.TaskChainRunnerService",
+  });
+  intent.putExtra(TCRS_LAUNCH_EVENT, params.launchEvent);
+  intent.putExtra(TCRS_EVENT_DATA, JSON.stringify(params.eventData));
+  if (params.eventId) {
+    intent.putExtra(TCRS_EVENT_ID, params.eventId);
+  }
+
+  return intent;
+}
+
+export function unpackAlarmRunnerServiceIntent(
+  intent: android.content.Intent
+): TaskChainRunnerParams {
+  if (!intent) {
+    return null;
+  }
+
+  const taskChainRunnerParams: TaskChainRunnerParams = {
+    launchEvent: intent.getStringExtra(TCRS_LAUNCH_EVENT),
+    eventData: JSON.parse(intent.getStringExtra(TCRS_EVENT_DATA)),
+  };
+
+  if (intent.hasExtra(TCRS_EVENT_ID)) {
+    taskChainRunnerParams.eventId = intent.getStringExtra(TCRS_EVENT_ID);
+  }
+
+  return taskChainRunnerParams;
+}
+
+const TCRS_LAUNCH_EVENT = "launchEvent";
+const TCRS_EVENT_DATA = "eventData";
+const TCRS_EVENT_ID = "eventId";
+
+interface TaskChainRunnerParams {
+  launchEvent: string;
+  eventData: EventData;
+  eventId?: string;
+}
