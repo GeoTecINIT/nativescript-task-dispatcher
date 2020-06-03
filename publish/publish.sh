009c422552a687aa0d6f6f40cd57de0d79ca5e0c
Alberto González <alberto.gonzalez@uji.es>
2020-06-03 19:30:39 +0200

    Embed npm publish set up inside publish script

--- publish/publish.sh
+++ publish/publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -v $NP_AUTH_TOKEN ]; then
+    echo "registry=https://registry.npmjs.com/" >> ~/.npmrc
+    echo "//registry.npmjs.com/:_authToken=$NPM_AUTH_TOKEN" >> ~/.npmrc
+fi
+
 PACK_DIR=package;
 
 publish() {
