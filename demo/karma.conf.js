403b9842c976c475c59e6fce40837bd4b8b2ae45
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-02-10 18:17:07 +0100

    Set up karma test reporter

--- demo/karma.conf.js
+++ demo/karma.conf.js
@@ -1,52 +1,39 @@
 module.exports = function (config) {
   const options = {
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
+    basePath: "",
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
+    frameworks: ["jasmine"],
 
     // list of files / patterns to load in the browser
-    files: ['app/tests/**/*.ts'],
-
+    files: ["app/tests/**/*.ts"],
 
     // list of files to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
+    preprocessors: {},
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ["progress", "junit"],
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
@@ -54,30 +41,35 @@ module.exports = function (config) {
 
     customLaunchers: {
       android: {
-        base: 'NS',
-        platform: 'android'
+        base: "NS",
+        platform: "android",
       },
       ios: {
-        base: 'NS',
-        platform: 'ios'
+        base: "NS",
+        platform: "ios",
       },
       ios_simulator: {
-        base: 'NS',
-        platform: 'ios',
-        arguments: ['--emulator']
-      }
+        base: "NS",
+        platform: "ios",
+        arguments: ["--emulator"],
+      },
     },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+
+    junitReporter: {
+      outputDir: "test-reports",
+      outputFile: "report.xml",
+    },
   };
 
   setWebpackPreprocessor(config, options);
   setWebpack(config, options);
 
   config.set(options);
-}
+};
 
 function setWebpackPreprocessor(config, options) {
   if (config && config.bundle) {
@@ -85,11 +77,11 @@ function setWebpackPreprocessor(config, options) {
       options.preprocessors = {};
     }
 
-    options.files.forEach(file => {
+    options.files.forEach((file) => {
       if (!options.preprocessors[file]) {
         options.preprocessors[file] = [];
       }
-      options.preprocessors[file].push('webpack');
+      options.preprocessors[file].push("webpack");
     });
   }
 }
@@ -100,10 +92,15 @@ function setWebpack(config, options) {
     env[config.platform] = true;
     env.sourceMap = config.debugBrk;
     env.appPath = config.appPath;
-    options.webpack = require('./webpack.config')(env);
+    options.webpack = require("./webpack.config")(env);
     delete options.webpack.entry;
     delete options.webpack.output.libraryTarget;
-    const invalidPluginsForUnitTesting = ["GenerateBundleStarterPlugin", "GenerateNativeScriptEntryPointsPlugin"];
-    options.webpack.plugins = options.webpack.plugins.filter(p => !invalidPluginsForUnitTesting.includes(p.constructor.name));
+    const invalidPluginsForUnitTesting = [
+      "GenerateBundleStarterPlugin",
+      "GenerateNativeScriptEntryPointsPlugin",
+    ];
+    options.webpack.plugins = options.webpack.plugins.filter(
+      (p) => !invalidPluginsForUnitTesting.includes(p.constructor.name)
+    );
   }
 }
