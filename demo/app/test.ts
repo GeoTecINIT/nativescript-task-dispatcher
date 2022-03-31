fb3edf30580265be88070c613fff92967d0ca8bf
Alberto González <10727467+agonper@users.noreply.github.com>
2022-03-31 18:18:37 +0100

    chore: lint config files

--- demo/app/test.ts
+++ demo/app/test.ts
@@ -4,8 +4,8 @@ import { runTestApp } from "@nativescript/unit-test-runner";
 declare let require: any;
 
 runTestApp({
-	runTests: () => {
-		const tests = require.context("./", true, /\.spec\.ts$/);
-		tests.keys().map(tests);
-	},
+    runTests: () => {
+        const tests = require.context("./", true, /\.spec\.ts$/);
+        tests.keys().map(tests);
+    },
 });
