8e106c34d166ed181098a314c1a3b3c9a3bad375
Alberto González <10727467+agonper@users.noreply.github.com>
2022-04-01 14:31:32 +0100

    feat: update boot receiver to handle schedule exact alarm permission granting

--- src/internal/tasks/schedulers/time-based/android/boot-receiver.android.ts
+++ src/internal/tasks/schedulers/time-based/android/boot-receiver.android.ts
@@ -1,11 +1,19 @@
 import { AndroidAlarmScheduler } from "./alarms/alarm/scheduler.android";
 import { getLogger } from "../../../../utils/logger";
+import { getExactAlarmPermsManager } from "./alarms/exact-alarm-perms-manager.android";
 
 export class BootReceiver
   implements es.uji.geotec.taskdispatcher.BootReceiverDelegate {
   onReceive(context: android.content.Context, intent: android.content.Intent) {
     const logger = getLogger("BootReceiver");
     logger.info("Performing boot initializations");
+
+    if (!getExactAlarmPermsManager().isGranted()) {
+      logger.error(
+        "Exact alarm permission not granted! Cancelling alarm setup"
+      );
+      return;
+    }
 
     const alarmScheduler = new AndroidAlarmScheduler();
     alarmScheduler
