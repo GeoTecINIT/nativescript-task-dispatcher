25cd161cc16e791af325cae7b58f2350b5965fd0
Alberto González <10727467+agonper@users.noreply.github.com>
2022-04-01 14:32:31 +0100

    fix: power savings manager to wait for user response to request

--- demo/app/tests/internal/tasks/schedulers/time-based/android/power-savings-manager.android.spec.ts
+++ demo/app/tests/internal/tasks/schedulers/time-based/android/power-savings-manager.android.spec.ts
@@ -1,39 +1,57 @@
 import { PowerSavingsManager } from "nativescript-task-dispatcher/internal/tasks/schedulers/time-based/android/alarms/power-savings-manager.android";
+import {createOsForegroundActivityMock} from "~/tests/internal/tasks/schedulers/time-based/android/index";
+import {Utils} from "@nativescript/core";
+import {
+    createSavingsDeactivationIntent
+} from "nativescript-task-dispatcher/internal/tasks/schedulers/time-based/android/intents.android";
 
 describe("Power savings manager", () => {
     if (typeof android === "undefined") {
         return;
     }
 
-    const osPowerManager = createOsPowerManagerMock();
+    const powerManagerMock = createOsPowerManagerMock();
+    const foregroundActivityMock = createOsForegroundActivityMock();
 
     beforeEach(() => {
-        spyOn(
-            osPowerManager,
-            "isIgnoringBatteryOptimizations"
-        ).and.callThrough();
+        spyOn(powerManagerMock, "isIgnoringBatteryOptimizations").and.returnValue(false);
+        spyOn(foregroundActivityMock, "startActivity");
     });
 
     it("checks if power savings are enabled", () => {
-        const powerSavingsManager = new PowerSavingsManager(osPowerManager);
+        const powerSavingsManager = new PowerSavingsManager(powerManagerMock);
 
         const areDisabled = powerSavingsManager.areDisabled();
 
-        expect(areDisabled === true || areDisabled === false).toBeTruthy();
+        expect(areDisabled).toBeFalse();
         expect(
-            osPowerManager.isIgnoringBatteryOptimizations
+            powerManagerMock.isIgnoringBatteryOptimizations
         ).toHaveBeenCalled();
     });
 
     it("returns true by default when api level is lower than 23", () => {
-        const powerSavingsManager = new PowerSavingsManager(osPowerManager, 22);
+        const powerSavingsManager = new PowerSavingsManager(powerManagerMock, 22);
 
         const areDisabled = powerSavingsManager.areDisabled();
 
         expect(areDisabled).toBeTruthy();
-        expect(
-            osPowerManager.isIgnoringBatteryOptimizations
-        ).not.toHaveBeenCalled();
+        expect(powerManagerMock.isIgnoringBatteryOptimizations).not.toHaveBeenCalled();
+    });
+
+    it("requests to disable savings when it has to", () => {
+        const powerSavingsManager = new PowerSavingsManager(
+            powerManagerMock,
+            android.os.Build.VERSION.SDK_INT,
+            () => foregroundActivityMock
+        );
+
+        powerSavingsManager.requestDeactivation();
+
+        expect(foregroundActivityMock.startActivity).toHaveBeenCalledOnceWith(
+            createSavingsDeactivationIntent(
+                Utils.android.getApplicationContext().getPackageName()
+            )
+        );
     });
 });
 
--- src/internal/tasks/schedulers/time-based/android/alarms/power-savings-manager.android.ts
+++ src/internal/tasks/schedulers/time-based/android/alarms/power-savings-manager.android.ts
@@ -1,6 +1,7 @@
 import { Application, Utils } from "@nativescript/core";
 import { createSavingsDeactivationIntent } from "../intents.android";
 import { Logger, getLogger } from "../../../../../utils/logger";
+import { waitForActivityResume } from "./perm-request-common";
 
 export class PowerSavingsManager {
   private logger: Logger;
@@ -11,27 +12,11 @@ export class PowerSavingsManager {
     private powerManager: android.os.PowerManager = Utils.android
       .getApplicationContext()
       .getSystemService(android.content.Context.POWER_SERVICE),
-    private skdVersion = android.os.Build.VERSION.SDK_INT
+    private skdVersion = android.os.Build.VERSION.SDK_INT,
+    private activityGetter = () => Application.android.foregroundActivity
   ) {
     this.logger = getLogger("PowerSavingsManager");
     this.appPackage = Utils.android.getApplicationContext().getPackageName();
-  }
-
-  // TODO: Evaluate what to do with devices not running Android Stock layer
-  requestDeactivation(): void {
-    if (this.askedOnce || this.areDisabled()) {
-      return;
-    }
-
-    if (!Application.android.foregroundActivity) {
-      this.logger.warn("Battery savings can not be enabled in background");
-
-      return;
-    }
-
-    this.askedOnce = true;
-    const intent = createSavingsDeactivationIntent(this.appPackage);
-    Application.android.foregroundActivity.startActivity(intent);
   }
 
   areDisabled(): boolean {
@@ -40,6 +25,34 @@ export class PowerSavingsManager {
     }
 
     return this.powerManager.isIgnoringBatteryOptimizations(this.appPackage);
+  }
+
+  // TODO: Evaluate what to do with devices not running Android Stock layer
+  async requestDeactivation(): Promise<void> {
+    if (this.askedOnce || this.areDisabled()) {
+      return;
+    }
+
+    const visibleActivity = this.activityGetter();
+    if (!visibleActivity) {
+      this.logger.warn("Battery savings can not be enabled in background");
+
+      return;
+    }
+    this.askedOnce = true;
+
+    const activityResume = waitForActivityResume();
+
+    const intent = createSavingsDeactivationIntent(this.appPackage);
+    visibleActivity.startActivity(intent);
+
+    await activityResume;
+
+    if (!this.areDisabled()) {
+      throw new Error(
+        "Disabling battery optimizations is required for the app to work as expected!"
+      );
+    }
   }
 }
 
